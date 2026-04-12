fe3a706b25a4a0a88118078bf99e35f026b76e79
YamenAmjed22 <yamenamjed22@gmail.com>
2026-04-12 12:43:29 +0300

    solve desgin

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -11,14 +11,29 @@ import { CommonModule } from '@angular/common';
 export class NavbarComponent {
   isScrolled = false;
   isMenuOpen = false;
+  isHidden = false;
+  lastScrollTop = 0;
 
   @HostListener('window:scroll', ['$event'])
   onWindowScroll(event: Event) {
-    if (window.scrollY > 50) {
+    const st = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (st > 50) {
       this.isScrolled = true;
     } else {
       this.isScrolled = false;
     }
+
+    if (st > this.lastScrollTop && st > 100) {
+      // Downscroll
+      this.isHidden = true;
+      this.isMenuOpen = false; // close menu if open while scrolling down
+    } else {
+      // Upscroll
+      this.isHidden = false;
+    }
+
+    this.lastScrollTop = st <= 0 ? 0 : st;
   }
 
   toggleMenu() {
